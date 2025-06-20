50e1d35178c23234332f8bdaa94affad99012b1f
Angie Jones <jones.angie@gmail.com>
2025-06-20 16:46:28 +0000

    02_03b: Making a GET Request

--- src/test/java/trainingxyz/ApiTests.java
+++ src/test/java/trainingxyz/ApiTests.java
@@ -5,6 +5,25 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 
 public class ApiTests {
+
+  String baseUrl = "http://localhost:8888/";
  
-  
+  @Test
+  public void getCategories(){
+    String endpoint = baseUrl + "category/read.php";
+    var response = given().when().get(endpoint).then();
+    response.log().body();
+  }  
+
+  @Test
+  public void getProduct(){
+    String endpoint = baseUrl + "product/read_one.php";
+    var response = 
+      given()
+        .queryParam("id", 2)
+      .when()
+        .get(endpoint)
+      .then();
+    response.log().body();
+  }
 }
